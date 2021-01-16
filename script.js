9c09f85512b938400a40a896904df4a337ce4449
Hoang <hoang.viet.nguyen@outlook.com>
2021-01-16 09:47:18 -0500

    Added google maps services initialization.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let map;
+let distanceMatrixService;
+let placeService;
+let directionsService;
+let directionsRenderer;
 
 function initMap() {
   let CanadaLocation = new google.maps.LatLng(56.1304, -106.3468);
@@ -6,4 +10,10 @@ function initMap() {
     center: CanadaLocation,
     zoom: 4,
   });
+
+  // Init Services
+  directionsService = new google.maps.DirectionsService();
+  directionsRenderer = new google.maps.DirectionsRenderer();
+  distanceMatrixService = new google.maps.DistanceMatrixService();
+  placeService = new google.maps.places.PlacesService(map);
 }
