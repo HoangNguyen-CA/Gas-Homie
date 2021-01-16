32769b2b61c210fbd35049edef1c45df0e363f92
Hoang <hoang.viet.nguyen@outlook.com>
2021-01-16 10:19:17 -0500

    Added rendering for path, Added nearbySearch to look for gas stations, Added distance matrix functionality to calculate distance between gas stations & start & end locations.

--- helper.js
+++ helper.js
@@ -9,3 +9,26 @@ function createMarker(place) {
     infowindow.open(map, marker);
   });
 }
+
+// helper function to parse  data from direction matrix responses
+function parseDistanceMatrix(response) {
+  var origins = response.originAddresses;
+  var destinations = response.destinationAddresses;
+
+  let ansArr = [];
+  for (var i = 0; i < origins.length; i++) {
+    var results = response.rows[i].elements;
+    for (var j = 0; j < results.length; j++) {
+      var element = results[j];
+      ans = {};
+      ans.distanceText = element.distance.text;
+      ans.durationText = element.duration.text;
+      ans.distance = element.distance.value; // in meters
+      ans.duration = element.duration.value; // in second
+      ans.from = origins[i];
+      ans.to = destinations[j];
+      ansArr.push(ans);
+    }
+  }
+  return ansArr;
+}
--- script.js
+++ script.js
@@ -8,6 +8,9 @@ let startInput = document.getElementById('startInput');
 let endInput = document.getElementById('endInput');
 let mainForm = document.getElementById('form');
 
+let startPlace;
+let endPlace;
+
 function initMap() {
   let CanadaLocation = new google.maps.LatLng(56.1304, -106.3468);
   map = new google.maps.Map(document.getElementById('map'), {
@@ -17,9 +20,10 @@ function initMap() {
 
   // Init Services
   directionsService = new google.maps.DirectionsService();
-  directionsRenderer = new google.maps.DirectionsRenderer();
   distanceMatrixService = new google.maps.DistanceMatrixService();
   placeService = new google.maps.places.PlacesService(map);
+  directionsRenderer = new google.maps.DirectionsRenderer();
+  directionsRenderer.setMap(map);
 
   // Init autocomplete fields
   var autocompleteOptions = {
@@ -39,6 +43,134 @@ function initMap() {
   mainForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    startAutocomplete.getPlace();
+    startPlace = startAutocomplete.getPlace();
+    endPlace = endAutocomplete.getPlace();
+
+    var searchRequest = {
+      location: startPlace.geometry.location,
+      radius: '5000',
+      query: '',
+      type: 'gas_station',
+    };
+
+    placeService.nearbySearch(searchRequest, nearbySearchCallback);
+
+    /*
+      Directions Request
+    */
+    let directionsRequest = {
+      origin: startPlace.geometry.location,
+      destination: endPlace.geometry.location,
+      travelMode: 'DRIVING',
+    };
+
+    directionsService.route(directionsRequest, directionsRequestCallback);
+
+    function directionsRequestCallback(result, status) {
+      if (status == 'OK') {
+        directionsRenderer.setDirections(result);
+      } else {
+        alert(
+          'Directions service was not successful for the following reason: ' +
+            status
+        );
+      }
+    }
   });
+}
+
+function nearbySearchCallback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      createMarker(place);
+    }
+
+    /*
+      GETTING DISTANCES USING DIRECTIONS MATRIX
+    */
+
+    transformedResults = results.map((res) => res.geometry.location);
+
+    // distance from start to each gas station
+    let distanceMatrixReq1 = {
+      origins: [startPlace.geometry.location],
+      destinations: transformedResults,
+      travelMode: 'DRIVING',
+    };
+    // distance from each gas station to end
+    let distanceMatrixReq2 = {
+      origins: transformedResults,
+      destinations: [endPlace.geometry.location],
+      travelMode: 'DRIVING',
+    };
+
+    distanceMatrixService.getDistanceMatrix(
+      distanceMatrixReq1,
+      distanceMatrixCallback1
+    );
+
+    //key is address of gas station
+    distanceDictionary = {};
+
+    function distanceMatrixCallback1(results, status) {
+      if (status == 'OK') {
+        let res = parseDistanceMatrix(results);
+        console.log(res);
+
+        // add response to distanceDictionary
+        for (item of res) {
+          distanceDictionary[item.to] = {
+            address: item.to,
+            distanceTo: item.distance,
+            durationTo: item.duration,
+          };
+        }
+
+        distanceMatrixService.getDistanceMatrix(
+          distanceMatrixReq2,
+          distanceMatrixCallback2
+        );
+      } else {
+        alert(
+          'distance matrix was not successful for the following reason: ' +
+            status
+        );
+      }
+    }
+
+    function distanceMatrixCallback2(results, status) {
+      if (status == 'OK') {
+        let res = parseDistanceMatrix(results);
+        console.log(res);
+
+        let updatedDict = { ...distanceDictionary };
+
+        for (item of res) {
+          prevDistance = distanceDictionary[item.from].distanceTo;
+          prevDuration = distanceDictionary[item.from].durationTo;
+          updatedDict[item.from] = {
+            ...distanceDictionary[item.from],
+            distanceFrom: item.distance,
+            durationFrom: item.duration,
+            totalDistance: item.distance + prevDistance,
+            totalDuration: item.duration + prevDuration,
+          };
+        }
+        distanceDictionary = updatedDict;
+
+        console.log(distanceDictionary);
+        //renderGasStations(Object.values(distanceDictionary));
+      } else {
+        alert(
+          'distance matrix was not successful for the following reason: ' +
+            status
+        );
+      }
+    }
+  } else {
+    alert(
+      'nearby search was not successful for the following reason: ' + status
+    );
+  }
 }
